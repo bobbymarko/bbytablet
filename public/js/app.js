9d28fdf2a4bcf837df604e792ca8cf617a77a4aa
bobbymarko <bobby@bobby-marko.com>
2011-12-15 23:00:57 -0600

    super menu

--- public/js/app.js
+++ public/js/app.js
@@ -22,12 +22,37 @@ $(document).ready(function(){
 	
   $('a[href^="#"]','.dropdown-menu').click(function(e){
     e.preventDefault();
-    
+    var parent = $(this).closest('.dropdown-menu');
     var primary = $(this).closest('.dropdown-primary-menu');
-    if (primary.length > 0) { //are we in the initial dropdown?
-      primary.hide();
-      $($(this).attr('href')).show();
-    }
+    
+    if ($(this).hasClass('dropdown-back')){
+      // we are moving back one level
+      primary = parent.children('.dropdown-primary-menu').first();
+      var secondary = $(this).closest('.dropdown-secondary-menu');
+      secondary.addClass('to-right');
+      primary.show().addClass('from-left');
+      parent.css({height:primary.height()});
+      
+      setTimeout(function(){ //timeout set to duration of animation
+        secondary.hide();
+        parent.css({height:'auto'});
+        $('.to-right').removeClass('to-right');
+        $('.from-left').removeClass('from-left');
+      },200);
+    }else if (primary.length > 0) { 
+      // we are in the initial dropdown
+      var target = $($(this).attr('href'));
+      primary.addClass('to-left');
+      parent.css({height:target.height()});
+      $($(this).attr('href')).show().addClass('from-right');
+      
+      setTimeout(function(){ //timeout set to duration of animation
+        primary.hide();
+        parent.css({height:'auto'});
+        $('.to-left').removeClass('to-left');
+        $('.from-right').removeClass('from-right');
+      },200);
+    } 
   });
 
 });
