d778a5a6681c3e5a2c7c164c339bdd4b9df98275
bobbymarko <bobbymarkodesign@gmail.com>
2012-02-02 14:43:46 -0600

    finishing landing pages

--- public/js/listing-logic.js
+++ public/js/listing-logic.js
@@ -179,15 +179,3 @@ function loadProducts(){
 		});
 	}
 }
-
-function getParameterByName(name)
-{
-	name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-	var regexS = "[\\?&]" + name + "=([^&#]*)";
-	var regex = new RegExp(regexS);
-	var results = regex.exec(window.location.href);
-	if(results == null)
-		return "";
-	else
-		return decodeURIComponent(results[1].replace(/\+/g, " "));
-}
