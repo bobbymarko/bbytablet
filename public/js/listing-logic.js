f67e654d67f43b663a652e39b0434ee087ce5e16
Bobby Marko <bobbymarkodesign@gmail.com>
2012-04-04 09:21:21 -0500

    adding commas to prices

--- public/js/listing-logic.js
+++ public/js/listing-logic.js
@@ -212,3 +212,23 @@ function loadProducts(){
 		});
 	}
 }
+
+function formatNumber(n) {
+    if (!isFinite(n)) {
+        return n;
+    }
+
+    var s = ""+n, abs = Math.abs(n), _, i;
+
+    if (abs >= 1000) {
+        _  = (""+abs).split(/\./);
+        i  = _[0].length % 3 || 3;
+
+        _[0] = s.slice(0,i + (n < 0)) +
+               _[0].slice(i).replace(/(\d{3})/g,',$1');
+
+        s = _.join('.');
+    }
+
+    return s;
+}
